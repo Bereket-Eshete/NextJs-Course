e15b7d3aa3baa65559529208dcce29e820bca1e7
Bereket-Eshete <bereketeshete63@gmail.com>
2025-08-31 21:06:28 +0300

    create navbar component and setup authurization using authjs

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+export default function Home() {
+  console.log("Hello, Next.js!");
+  return (
+    <>
+      <Navbar />
+      <h1>Home</h1>
+    </>
+  );
+}
--- app/components/hello.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-export default function Hello() {
-  console.log("I am a client side component");
-  return <h2>Hello from the Hello component!</h2>;
-}
